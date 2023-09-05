035aa51f33e1a61c5d85d00ae075a864858e6d49
kento <kentoyamamoto26@gmail.com>
2023-09-05 19:49:20 +0900

    add ThreeImageToTensor class

--- train_Resnet18.py
+++ train_Resnet18.py
@@ -54,6 +54,53 @@ class TensorImageDataset(Dataset):
             l = pickle.load(label)
         return l
 
+#front,left,rightの3方向のカメラ画像をDatasetにするクラス
+class ThreeImageToTensorDataset(Dataset):
+    #label_fileは"/data内のtestかtrainのパス、img_dirは画像があるフォルダのパスにする
+    def __init__(self, label_file, img_dir, transform=None, target_transform=None) -> None:
+        self.img_labels = self._open_label_data(label_file) 
+        self.img_paths = self._get_img_paths(img_dir)
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)-IMAGE_NUM
+    
+    def __getitem__(self, idx):
+        #image_path_tuple = tuple(read_image(self.img_paths[i], mode=ImageReadMode.RGB).to(torch.float32) for i in range(idx,idx+IMAGE_NUM))
+        img_list = []
+        for i in range(idx, idx+IMAGE_NUM):
+            for img in self.img_paths[i]:
+                img_list.append(read_image(img, mode=ImageReadMode.RGB).to(torch.float32))
+        images = torch.concat(img_list)
+        label_set = set(torch.tensor(self.img_labels[i]) for i in range(idx,idx+IMAGE_NUM))
+        label = int(1 in label_set) #4フレーム中に一つでも１があったら（１フレームでも信号機が赤になっていたら）１になる
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return images, label
+    
+    def _get_img_paths(self, img_dir):
+        img_dir = os.path.abspath(img_dir)
+        img_paths = [img_dir+"/"+p for p in sorted(os.listdir(img_dir)) if os.path.splitext(p)[1] in [".jpg", ".jpeg", ".png", ".bmp"]]
+
+        front_path = [f for f in img_paths if "front" in f]
+        left_path = [f for f in img_paths if "left" in f]
+        right_path = [f for f in img_paths if "right" in f]
+        
+        cam_path_list = []
+        for i in range(min(len(front_path),min(len(right_path),len(left_path)))):
+            cam_path_list.append([left_path[i],front_path[i],right_path[i]])
+       
+        return cam_path_list
+    
+    def _open_label_data(self,label_file):
+        abs_label_path = os.path.abspath(label_file)
+        with open(abs_label_path,"rb") as label:
+            l = pickle.load(label)
+        return l
+
 
 def train(dataloader, model, loss_fn, optimizer, on_write:bool=False):
     size = len(dataloader.dataset)
